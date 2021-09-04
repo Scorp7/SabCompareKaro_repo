447456afcc1902fc7ad27ee4c7354672e3e1f1c2
abhishekmehta22 <abhishek.mo.er.m@gmail.com>
2021-09-05 01:09:19 +0530

    Worked on login and signup page

--- Project/user/urls.py
+++ Project/user/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('login/', views.login, name='login_form'),
+    path('', views.home, name='home_page'),
+    
 ]
